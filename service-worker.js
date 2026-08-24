ca3f0d727def89e33872d33ca3f0a0f21a44d68e
Kaef-1o2 <bayk102@gmail.com>
2026-08-24 16:28:01 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,129 @@
+/* ============================================================
+   EMBELIS PLANNING - SERVICE WORKER
+============================================================ */
+
+const CACHE_NAME =
+    "embelis-planning-v1";
+
+
+const APP_FILES = [
+
+    "./",
+    "./index.html",
+    "./manifest.webmanifest",
+    "./icons/icon-192.png",
+    "./icons/icon-512.png"
+
+];
+
+
+/* ============================================================
+   INSTALLATION
+============================================================ */
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches
+                .open(
+                    CACHE_NAME
+                )
+                .then(
+                    cache =>
+                        cache.addAll(
+                            APP_FILES
+                        )
+                )
+
+        );
+
+
+        self.skipWaiting();
+
+    }
+);
+
+
+/* ============================================================
+   ACTIVATION
+============================================================ */
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches
+                .keys()
+                .then(
+                    cacheNames =>
+                        Promise.all(
+
+                            cacheNames
+                                .filter(
+                                    name =>
+                                        name !==
+                                        CACHE_NAME
+                                )
+                                .map(
+                                    name =>
+                                        caches.delete(
+                                            name
+                                        )
+                                )
+
+                        )
+                )
+
+        );
+
+
+        self.clients.claim();
+
+    }
+);
+
+
+/* ============================================================
+   REQUETES
+============================================================ */
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        /*
+           On ne gère ici que les requêtes GET.
+           Supabase continue de fonctionner normalement.
+        */
+
+        if(
+            event.request.method !==
+            "GET"
+        ){
+
+            return;
+
+        }
+
+
+        event.respondWith(
+
+            fetch(
+                event.request
+            )
+            .catch(
+                () =>
+                    caches.match(
+                        event.request
+                    )
+            )
+
+        );
+
+    }
+);
